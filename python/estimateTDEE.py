85e219b862cd5fc05af4ea94b2c16cf7069dec7f
Jan Zwiener <jan@zwiener.org>
2024-02-14 05:21:42 +0100

    WIP

--- python/estimateTDEE.py
+++ python/estimateTDEE.py
@@ -34,6 +34,7 @@ def estimate_tdee():
     filter_state = np.array([[initial_weight], [tdee_begin]])
     filter_state_P = np.diag([std_weight_measurement**2, tdee_begin_stddev**2])
     R = std_weight_measurement**2
+    questionable_measurements = 0; # keep track of prediction vs. measurement mismatches
 
     # Sort by startDate if not already sorted
     bodymass.sort_values(by='startDate', inplace=True)
@@ -44,9 +45,8 @@ def estimate_tdee():
     history_weight_raw = np.zeros(epochs)
     history_kcals = np.zeros(epochs)
     history_dt_in_days = np.zeros(epochs)
-
     # Kalman Filter and RTS Smoother placeholders
-    n = 2  # State vector dimension
+    n = filter_state.shape[0]  # State vector dimension
     rts_history_filter_state_apriori = np.zeros((n, epochs))
     rts_history_filter_state_aposteriori = np.zeros((n, epochs))
     rts_history_filter_state_P_apriori = np.zeros((n, n, epochs))
@@ -54,33 +54,54 @@ def estimate_tdee():
     rts_history_phi = np.zeros((n, n, epochs))
 
     for i in range(epochs):
-        # Time delta in days
-        dt_date = (bodymass['startDate'].iloc[i+1] - bodymass['startDate'].iloc[i])
-        dt = dt_date.total_seconds()/(3600.0*24)
-
+        # bookkeeping for plots
+        current_body_mass = bodymass['value'].iloc[i] # mass at epoch i
         history_time_in_days[i] = (bodymass['startDate'].iloc[i] - bodymass['startDate'].iloc[0]).days
-        history_weight_raw[i] = bodymass['value'].iloc[i]
-        history_dt_in_days[i] = dt
+        history_weight_raw[i] = current_body_mass
 
+        # Kalman Filter Fusion / Correction with current body mass measurement
+        # --------------------------------------------------------------------
+        rts_history_filter_state_apriori[:, i] = filter_state.flatten()
+        rts_history_filter_state_P_apriori[:,:, i] = filter_state_P
+
+        H = np.array([[1, 0]]) # Kalman Filter Design matrix
+        # dl is the delta between the measurement and the expected body mass
+        dl = np.array([[current_body_mass - filter_state[0, 0]]])
+        filter_state, filter_state_P, outlier = kalman_robust(filter_state, filter_state_P, dl, R, H, chi2_threshold=5.99)
+        if outlier:
+            questionable_measurements += 1
+
+        rts_history_filter_state_aposteriori[:, i] = filter_state.flatten()
+        rts_history_filter_state_P_aposteriori[:,:, i] = filter_state_P
+
+        # Predict to next epoch
+        # ---------------------
         # Energy consumed calculation
         start_date = bodymass['startDate'].iloc[i]
         end_date = bodymass['startDate'].iloc[i+1]
+        dt_date = (end_date - start_date)
+        dt = dt_date.total_seconds()/(3600.0*24) # prediction step in days (float)
+        assert(dt >= 0.0)
+        history_dt_in_days[i] = dt # bookkeeping for plots
 
+        # sum up all calories consumed in the prediction horizon
         kcal_in_interval = energyconsumed[(energyconsumed['startDate'] >= start_date) & (energyconsumed['startDate'] < end_date)]['value'].sum()
-        history_kcals[i] = kcal_in_interval
+        history_kcals[i] = kcal_in_interval # bookkeeping for plots
 
-        # Kalman filter update
-        H = np.array([[1, 0]])
-        dl = np.array([[bodymass['value'].iloc[i] - filter_state[0, 0]]])
-        filter_state, filter_state_P, _ = kalman_robust(filter_state, filter_state_P, dl, R, H, chi2_threshold=5.99)  # Example chi2_threshold
+        # try to  highlight periods without proper food tracking
+        prediction_penalty_kg = 0
+        if dt >= 1.0: # if for a longer period of time,
+            if kcal_in_interval < 0.5*filter_state[1, 0]*dt: # the tracked calories is way below the TDEE, print a note to the console
+                print("Note: Low food energy consumption on day %i" % round(history_time_in_days[i]))
+                prediction_penalty_kg = 0.25; # assume that the predicted body mass is a bit more inaccurate
 
-        # State transition and prediction
+        # Kalman state transition and prediction for next epoch
         phi = np.array([[1, -dt / kcal_per_kg_fat], [0, 1]])
         u = kcal_in_interval
         G = np.array([[1 / kcal_per_kg_fat], [0]])
         std_kcal = kcal_tracking_precision * dt
         Qu = std_kcal**2
-        Qnoise = np.diag([pred_noise_kg**2 * dt**2, pred_noise_tdee**2 * dt**2])
+        Qnoise = np.diag([((pred_noise_kg + prediction_penalty_kg)*dt)**2, (pred_noise_tdee*dt)**2])
 
         filter_state = phi @ filter_state + G * u
         filter_state_P = phi @ filter_state_P @ phi.T + G * Qu * G.T + Qnoise
@@ -108,22 +129,26 @@ def estimate_tdee():
     # Plot mass
     plt.plot(history_time_in_days, filter_state_rts[0, :], 'b', label='Body Mass (kg)')
     plt.ylabel('Mass (kg)')
-    plt.gca().set_prop_cycle(None)  # Reset the color cycle to reuse the first color for yyaxis right
+    plt.gca().set_prop_cycle(None)  # Reset the color cycle to reuse the first color for y-axis right
+    ax1 = plt.gca()
 
     # Create secondary y-axis for TDEE
     ax2 = plt.gca().twinx()
     ax2.plot(history_time_in_days, filter_state_rts[1, :], 'r-', label='TDEE (kcal)')
-    ax2.plot(history_time_in_days, filter_state_rts[1, :] + std_tdee, 'k--', label='1 sigma band TDEE')
-    ax2.plot(history_time_in_days, filter_state_rts[1, :] - std_tdee, 'k--')
+    # ax2.plot(history_time_in_days, filter_state_rts[1, :] + std_tdee, 'k--', label='1 sigma band TDEE')
+    # ax2.plot(history_time_in_days, filter_state_rts[1, :] - std_tdee, 'k--')
+    ax2.fill_between(history_time_in_days,
+                     filter_state_rts[1, :] - std_tdee,
+                     filter_state_rts[1, :] + std_tdee,
+                     color='red', alpha=0.15, label='1 σ error band')
     ax2.set_ylabel('TDEE (kcal)')
-
     # Adjust the y-limit for TDEE
-    ax2.set_ylim(0, np.max(filter_state_rts[1, :] + std_tdee) * 1.2)
-
+    ax2.set_ylim(np.min(filter_state_rts[1, :] - std_tdee) * 0.9, np.max(filter_state_rts[1, :] + std_tdee) * 1.1)
     # Because we are using twin axes, we need to manually construct the legend to include all lines
-    lines, labels = plt.gca().get_legend_handles_labels()
+
+    lines, labels = ax1.get_legend_handles_labels()
     lines2, labels2 = ax2.get_legend_handles_labels()
-    ax2.legend(lines + lines2, labels + labels2, loc='best')
+    ax2.legend(lines + lines2, labels + labels2, loc='lower right')
 
     plt.show()
 
